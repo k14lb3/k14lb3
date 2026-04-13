91a6e445b3dbeb975dba82496b4b2fb1f76c11d2
k14lb3 <k14lb3@gmail.com>
2026-04-13 23:43:22 +0800

    🍱 Add URLS constant

---
+++ src/constants/urls.ts
@@ -0,0 +1,8 @@
+export const URLS = {
+  RESUME: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&autoplay=1',
+  GITHUB: 'https://github.com/k14lb3/',
+  LINKEDIN: 'https://www.linkedin.com/in/karlivanalberto/',
+  ARCH_LINUX: 'https://archlinux.org/',
+  NEOVIM: 'https://neovim.io/',
+  BTW_I_USE_ARCH_MEME: 'https://knowyourmeme.com/memes/btw-i-use-arch',
+};
