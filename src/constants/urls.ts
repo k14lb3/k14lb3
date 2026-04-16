1e11cd0d4721d900fc61bc8c6fefd04a62a01040
k14lb3 <k14lb3@gmail.com>
2026-04-16 22:53:21 +0800

    🍱 Add URLs for 95 project

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -3,4 +3,6 @@ export const URLS = {
   GITHUB: 'https://github.com/k14lb3/',
   LINKEDIN: 'https://www.linkedin.com/in/karlivanalberto/',
   BTW_I_USE_ARCH_MEME: 'https://knowyourmeme.com/memes/btw-i-use-arch',
+  95: 'https://github.com/k14lb3/95',
+  '95_GITHUB': 'https://github.com/k14lb3/95',
 };
