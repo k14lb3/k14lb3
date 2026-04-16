4a8aa3cdf2540473cf5a040e5b5916ecd78d9ead
k14lb3 <k14lb3@gmail.com>
2026-04-16 23:58:17 +0800

    ♻️ Remove trailing / in URLs

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,7 +1,7 @@
 export const URLS = {
   RESUME: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&autoplay=1',
-  GITHUB: 'https://github.com/k14lb3/',
-  LINKEDIN: 'https://www.linkedin.com/in/karlivanalberto/',
+  GITHUB: 'https://github.com/k14lb3',
+  LINKEDIN: 'https://www.linkedin.com/in/karlivanalberto',
   BTW_I_USE_ARCH_MEME: 'https://knowyourmeme.com/memes/btw-i-use-arch',
   95: 'https://95-orpin.vercel.app',
   '95_GITHUB': 'https://github.com/k14lb3/95',
