65f54c3468163ff580417da9f59d775edd5130e7
k14lb3 <k14lb3@gmail.com>
2026-04-16 23:57:36 +0800

    🍱 Update 95 URL

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -3,6 +3,6 @@ export const URLS = {
   GITHUB: 'https://github.com/k14lb3/',
   LINKEDIN: 'https://www.linkedin.com/in/karlivanalberto/',
   BTW_I_USE_ARCH_MEME: 'https://knowyourmeme.com/memes/btw-i-use-arch',
-  95: 'https://github.com/k14lb3/95',
+  95: 'https://95-orpin.vercel.app',
   '95_GITHUB': 'https://github.com/k14lb3/95',
 };
