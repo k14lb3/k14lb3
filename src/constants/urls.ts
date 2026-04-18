93f0d6032457c6a304148f635913ce49ed0af654
k14lb3 <k14lb3@gmail.com>
2026-04-18 20:44:37 +0800

    🍱 Add resume

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,5 +1,5 @@
 export const URLS = {
-  RESUME: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&autoplay=1',
+  RESUME: '/resume.pdf',
   GITHUB: 'https://github.com/k14lb3',
   LINKEDIN: 'https://www.linkedin.com/in/karlivanalberto',
   BTW_I_USE_ARCH_MEME: 'https://knowyourmeme.com/memes/btw-i-use-arch',
